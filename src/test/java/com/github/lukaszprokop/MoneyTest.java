b2c78f30a4daa89e2c53235aae5cb30c14e053ff
lukasz <wuzu222@gmail.com>
2021-03-11 11:35:37 +0100

    Dodaj metodę zamieniającą waluty

--- src/test/java/com/github/lukaszprokop/MoneyTest.java
+++ src/test/java/com/github/lukaszprokop/MoneyTest.java
@@ -21,13 +21,6 @@ public class MoneyTest {
     }
 
     @Test
-    public void testFrancMultiplication(){
-        Money five = Money.franc(5);
-        assertEquals(five.times(2), Money.franc(10));
-        assertEquals(five.times(3), Money.franc(15));
-    }
-
-    @Test
     public void testCurrency(){
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
@@ -35,7 +28,17 @@ public class MoneyTest {
 
     @Test
     public void testDifferentClassEquality(){
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+        assertTrue(new Money(10, "CHF").equals(new Money(10, "CHF")));
+    }
+
+    @Test
+    public void testSimpleAddition(){
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        assertEquals(Money.dollar(10), sum);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(10), reduced);
     }
 
 }
